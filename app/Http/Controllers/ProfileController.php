070cdce052d3f2fc7470f4606b9f8afc84850eca
Musab Ismael <knightkirito6@gmail.com>
2024-08-26 15:50:00 +0300

    Fixing 5 points

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
@@ -14,6 +16,13 @@ class ProfileController extends Controller
     public function update(ProfileUpdateRequest $request)
     {
         // Task: fill in the code here to update name and email
+
+        $user = Auth::user();
+        $data = ['name' => $request['name'],'email' => $request['email']];
+        if ($request['password']) {
+            $data['password'] = bcrypt($request['password']);
+        }
+        $user->update($data);
         // Also, update the password if it is set
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
